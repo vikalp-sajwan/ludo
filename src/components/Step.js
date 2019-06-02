c6e6c64855d4c6775d5cf7790cee81a17b992cd3
vikalpsajwan <itsvikalp@gmail.com>
2019-06-02 23:16:01 +0530

    Add star step and position piece

--- src/components/Step.js
+++ src/components/Step.js
@@ -3,22 +3,35 @@ import styled from 'styled-components';
 
 import Piece from './Piece';
 import { theme } from '../theme';
+import starImg from '../images/star.svg';
 
 const StepTile = styled.div`
   width: 100px;
   height: 100px;
   display: grid;
   align-items: center;
+  justify-items: center;
   border: 5px solid rgba(0, 0, 0, 0.03);
   border-radius: 3px;
+  box-sizing: border-box;
   background-color: ${props => theme.base[props.color]};
+
+  ${({ showStar }) =>
+    showStar &&
+    `
+    background: url(${starImg}) no-repeat;
+  `}
 `;
 
 const Step = props => {
-  const { color = 'white', pieces = [] } = props;
+  const { color = 'white', showStar = false, pieces = [] } = props;
   const pieceItems = pieces.map(pieceColor => <Piece color={pieceColor} />);
 
-  return <StepTile color={color}>{pieceItems}</StepTile>;
+  return (
+    <StepTile color={color} showStar={showStar}>
+      {pieceItems}
+    </StepTile>
+  );
 };
 
 export default Step;
--- src/components/Steps.stories.js
+++ src/components/Steps.stories.js
@@ -7,4 +7,5 @@ storiesOf('Step', module)
   .add('normal', () => <Step />)
   .add('colored', () => <Step color="red" />)
   .add('with piece', () => <Step pieces={['yellow']} />)
-  .add('colored with piece', () => <Step color="blue" pieces={['yellow']} />);
+  .add('colored with piece', () => <Step color="blue" pieces={['yellow']} />)
+  .add('star step', () => <Step showStar={true} />);
