235539728cbd7be21d923629fd31a7a14a7ce2d3
vikalpsajwan <itsvikalp@gmail.com>
2019-09-21 01:41:51 +0530

    Add redux and render app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import Board from './components/Board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const mapStateToProps = state => {
+  return { state: state };
+};
 
-export default App;
+const App = ({ state }) => {
+  return <Board gameState={state} />;
+};
+
+export default connect(mapStateToProps)(App);
