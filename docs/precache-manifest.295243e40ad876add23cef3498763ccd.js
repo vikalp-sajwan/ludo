7189d62fe77452dbe4fc6f6aa4d08104dd1a1a1c
vikalpsajwan <itsvikalp@gmail.com>
2020-05-06 05:40:28 +0530

    Add static storybook site

---
+++ docs/precache-manifest.295243e40ad876add23cef3498763ccd.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d103e0b12a1adf90db782c368e333b7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.977331347caa07cf54ab.bundle.js"
+  },
+  {
+    "url": "runtime~main.977331347caa07cf54ab.bundle.js"
+  },
+  {
+    "revision": "7aeab346a0fbd26c670f43600f52495e",
+    "url": "static/media/star.7aeab346.svg"
+  },
+  {
+    "url": "vendors~main.977331347caa07cf54ab.bundle.js"
+  }
+]);
